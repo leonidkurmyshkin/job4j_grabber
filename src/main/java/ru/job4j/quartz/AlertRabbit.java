d15c64d4ad7753af0e812cbf6da73e46f75db463
leonidkurmyshkin <leonid2073@gmail.com>
2021-10-15 00:31:06 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,11 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -11,6 +16,13 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
+    public static Connection initConnection(Properties config) throws Exception {
+        Class.forName(config.getProperty("driver-class-name"));
+        return DriverManager.getConnection(
+                config.getProperty("url"),
+                config.getProperty("username"),
+                config.getProperty("password"));
+    }
 
     public static Properties readConfig(String fileName) throws IOException {
         try (var in = AlertRabbit.class.getClassLoader().getResourceAsStream(fileName)) {
@@ -23,27 +35,51 @@ public class AlertRabbit {
     }
 
     public static class Rabbit implements Job {
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            var store = (Connection) context.getJobDetail().getJobDataMap().get("store");
+            try (var ps = store.prepareStatement(
+                    "INSERT INTO rabbit(created_date) VALUES (?);")) {
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    public static Scheduler schedulerStart(Connection cn, Properties config) throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        JobDataMap store = new JobDataMap();
+        store.put("store", cn);
+        JobDetail job = newJob(Rabbit.class)
+                .usingJobData(store)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(config.getProperty("rabbit.interval")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+        return scheduler;
     }
 
     public static void main(String[] args) {
         try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(
-                            readConfig("rabbit.properties")
-                            .getProperty("rabbit.interval")))
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-            scheduler.start();
+            Properties config = readConfig("rabbit.properties");
+            try (Connection cn = initConnection(config)) {
+                Scheduler scheduler = schedulerStart(cn, config);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
