b088a4a2f0e62cf9620bc972a9c140bacd259d33
leonidkurmyshkin <leonid2073@gmail.com>
2021-10-19 14:53:58 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,22 +5,33 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.function.Predicate;
+
 public class SqlRuParse {
-    public static void printSitePage(String url) throws Exception {
+    public static void printVacancy(String url) throws Exception {
+        Document doc = Jsoup.connect(url).get();
+        Element vacancy = doc.selectFirst(".msgTable");
+        String title = vacancy.selectFirst(".messageHeader").ownText();
+        String description = vacancy.select(".msgBody").get(1).text();
+        String created = vacancy.selectFirst(".msgFooter").ownText()
+                .split("\s*\\[", 2)[0];
+        System.out.printf("%s%n%s%n%s%n%s%n%n", title, description, url, created);
+    }
+
+    public static void printSitePage(String url, Predicate<String> skip) throws Exception {
         Document doc = Jsoup.connect(url).get();
         Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(td.parent().child(5).text());
+            if (skip.test(td.ownText())) {
+                printVacancy(td.child(0).attr("href"));
+            }
         }
     }
 
     public static void main(String[] args) throws Exception {
         var startUrl = "https://www.sql.ru/forum/job-offers/%d";
-        for (var i = 1; i <= 5; i++) {
-            printSitePage(startUrl.formatted(i));
+        for (var i = 2; i <= 2; i++) {
+            printSitePage(startUrl.formatted(i), el -> !"Важно:".equals(el));
         }
     }
 }
